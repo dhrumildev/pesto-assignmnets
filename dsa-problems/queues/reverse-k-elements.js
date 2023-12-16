1aff1c3f769a9ae7cb9fb06a25b49494ccfc8b12
dhrumildev <dhrumilshah280394@gmail.com>
2023-12-16 23:36:22 +0530

    next grater ele

--- dsa-problems/queues/reverse-k-elements.js
+++ dsa-problems/queues/reverse-k-elements.js
@@ -1,3 +1,12 @@
+// Problem Statement:
+// You are given a queue of integers and a positive integer K. Your task is to reverse the order of the first K elements in the queue, while keeping the remaining elements in the same order.
+
+// Write a function reverseKElements(queue, K) that takes two parameters:
+
+// queue: An array representing the queue of integers. K: A positive integer indicating the number of elements to be reversed. The function should return the modified queue after reversing the first K elements.
+
+
+
 function reverseKElements(queue, K) {
     if (!Array.isArray(queue) || K <= 0 || K > queue.length) {
         // Invalid input, return the original queue
@@ -18,4 +27,4 @@ const inputQueue = [1, 2, 3, 4, 5, 6, 7];
 const K = 3;
 const result = reverseKElements(inputQueue, K);
 
-console.log(result); // Output: [3, 2, 1, 4, 5, 6, 7]
+console.log(result); 
