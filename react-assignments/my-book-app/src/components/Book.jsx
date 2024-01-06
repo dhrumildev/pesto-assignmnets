61cd2e41dabb4d9f9fb852eb6fc5d7357d6d9f15
dhrumildev <dhrumilshah280394@gmail.com>
2024-01-07 01:09:13 +0530

    commit assignment 2

--- react-assignments/my-book-app/src/components/Book.jsx
+++ react-assignments/my-book-app/src/components/Book.jsx
@@ -1,14 +1,31 @@
-// Book.js
-import React from 'react';
+import React, { PureComponent } from 'react';
+import BookDetail from './BookDetails.jsx';
 
-const Book = ({ title, author, year }) => {
+class Book extends PureComponent {
+  render() {
+    const { title, author, year } = this.props;
+
     return (
-        <div style={{"text-align":"left"}}>
-            <h3>{title}</h3>
-            <p>Author: {author}</p>
-            <p>Year: {year}</p>
-        </div>
+      <div style={{"textAlign":"left"}}>
+        <BookDetail title={title} author={author} year={year} />
+      </div>
     );
-};
+  }
+}
 
 export default Book;
+
+
+// import React from 'react';
+
+// const Book = ({ title, author, year }) => {
+//     return (
+//         <div style={{"text-align":"left"}}>
+//             <h3>{title}</h3>
+//             <p>Author: {author}</p>
+//             <p>Year: {year}</p>
+//         </div>
+//     );
+// };
+
+// export default Book;
--- react-assignments/my-book-app/src/components/BookList.jsx
+++ react-assignments/my-book-app/src/components/BookList.jsx
@@ -1,6 +1,33 @@
+// import React from 'react';
+// import Book from './Book.jsx';
+
+// const books = [
+//   { title: 'Book 1', author: 'Author 1', year: 2020 },
+//   { title: 'Book 2', author: 'Author 2', year: 2018 },
+//   { title: 'Book 3', author: 'Author 3', year: 2022 },
+//   // Add more books if you'd like
+// ];
+
+// const BookList = () => {
+//   return (
+//     <div>
+//       <h2>Book List</h2>
+//       <ul>
+//         {books.map((book, index) => (
+//           <Book key={index} {...book} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default BookList;
+
 // BookList.js
-import React from 'react';
-import Book from './Book.jsx';
+import React, { Component } from 'react';
+import Book from './Book';
+import WithLogging from './WithLogging.jsx'; // Import the WithLogging HOC
+
 
 const books = [
   { title: 'Book 1', author: 'Author 1', year: 2020 },
@@ -9,17 +36,19 @@ const books = [
   // Add more books if you'd like
 ];
 
-const BookList = () => {
-  return (
-    <div>
-      <h2>Book List</h2>
-      <ul>
-        {books.map((book, index) => (
-          <Book key={index} {...book} />
-        ))}
-      </ul>
-    </div>
-  );
-};
+class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Book List</h2>
+        <ul>
+          {books.map((book, index) => (
+            <Book key={index} {...book} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
-export default BookList;
+export default WithLogging(BookList);
