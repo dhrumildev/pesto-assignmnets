f256764778cf6c9465ea03dc295bedfb11763ab0
dhrumildev2894 <djdonmil@gmail.com>
2024-02-10 23:47:38 +0530

    react router assignment

--- react-assignments/portfolio-website/src/components/Home/Home.jsx
+++ react-assignments/portfolio-website/src/components/Home/Home.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import './HomePage.css';
+import { Link } from 'react-router-dom';
 
 
 const HomePage = () => {
@@ -11,26 +12,30 @@ const HomePage = () => {
                 <h1 className="display-3">Dhrumil Shah</h1>
                 <p className="lead">Sr. Software Engineer</p>
             </header>
-            <body>
-                <section className="intro-section text-center">
-                    <p>
-                        Welcome to my portfolio! I'm a passionate software engineer with expertise
-                        in back-end and front-end technologies.
+
+            <section className="intro-section text-center">
+                <p>
+                    As a Senior Fullstack Developer with experience in both enterprise-level and startup environments, I have a proven track record of successfully developing and deploying blockchain solutions. With expertise in technologies such as JavaScript, NodeJS, Angular, ReactJS, Hyperledger Fabric, AWS, GCP, Azure, Solidity, and more, I have the skills necessary to design and implement secure and scalable full stack applications. I have also demonstrated strong leadership and technical leading abilities, mentoring junior developers and leading successful projects. Accomplishments include being recognized as Employee of the Year and Employee of the Month multiple times, as well as achieving high rankings in technical competitions. I am now seeking a challenging role as a Sr. Software Developer where I can continue to grow and contribute to the success of a dynamic organization.
         </p>
-                    <p>
-                        <img src="/Users/dhrumilshah/pesto-assignmnets/react-assignments/portfolio-website/src/components/Home/code_20240127_015001_via_10015_io.png" alt="Dhrumil Skills" />
-                    </p>
+                <p>
+                    <img src="myskills.png" alt="Dhrumil Skills" />
+                </p>
+                <Link to={`/projects`}>
                     <button className="btn btn-primary mr-2">Projects</button>
+                </Link>
+                <Link to={`/contact`}>
                     <button className="btn btn-primary mx-2">Contact</button>
+
+                </Link>
+                <Link to={'/skills'}>
                     <button className="btn btn-primary m1-2">Skills</button>
+                </Link>
+            </section>
 
-                </section>
-
-            </body>
 
 
             <footer className="text-center mt-4">
-                <p>&copy; 2024 Dhrumil Shah</p>
+                <p className="text-muted">&copy; 2024 DHRUMIL SHAH</p>
             </footer>
         </div>
     );
